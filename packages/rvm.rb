e828525f0a5955b7e32e5b1524b92d938cde8279
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-10-09 14:42:44 -0500

    updated rvm package

--- packages/rvm.rb
+++ packages/rvm.rb
@@ -6,8 +6,10 @@ package :rvm do
   description "RVM"
   requires :rvm_dependencies
 
-  apt "ruby-full" do
-    post :install, "-u deploy gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
-    post :install, "curl -L https://get.rvm.io | bash -s stable"
+  runner "-u deploy gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+  runner "curl -L https://get.rvm.io | bash -s stable"
+
+  verify do
+    has_executable "/home/deploy/.rvm/bin/rvm"
   end
 end
