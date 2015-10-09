adad43ec0e8e26208cf74a39f8641d301c959fb4
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-10-09 13:13:52 -0500

    added sprinkle file and ruby_deps package

---
+++ packages/ruby_dependencies.rb
@@ -0,0 +1,4 @@
+package :ruby_dependencies do
+  description "ruby dependencies"
+  apt %w(curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev)
+end
---
+++ sprinkle.rb
@@ -0,0 +1,20 @@
+Dir["./sprinkles/packages/*.rb"].each { |f| require f }
+
+policy :ruby, roles: :app do
+  requires :ruby_dependencies
+end
+
+deployment do
+  delivery :ssh do
+    user "deploy"
+    use_sudo true
+
+    role :app, "123.123.123.123"
+  end
+
+  source do
+    prefix   "/usr/local"
+    archives "/usr/local/sources"
+    builds   "/usr/local/build"
+  end
+end
