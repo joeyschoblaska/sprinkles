da8e3ef3eaced6e5e889dcbb093a5683fe20b03f
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-11-13 18:56:00 -0600

    add verification for nginx

--- packages/nginx.rb
+++ packages/nginx.rb
@@ -7,4 +7,8 @@ package :nginx do
   apt %w(nginx), sudo: true
   file "/etc/nginx/sites-available/#{@app_name}", content: render("nginx.conf"), sudo: true
   runner "ln -s /etc/nginx/sites-available/#{@app_name} /etc/nginx/sites-enabled/#{@app_name}", sudo: true
+
+  verify do
+    has_executable "nginx"
+  end
 end
