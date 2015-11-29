5b821b248895011d3e925ad5c80566c874a21657
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-11-28 22:12:44 -0600

    dotfiles can't require ruby without specifying version

--- packages/dotfiles.rb
+++ packages/dotfiles.rb
@@ -1,6 +1,5 @@
 package :dotfiles do
   description "joeyschoblaska/dotfiles.git"
-  requires :ruby
 
   runner "if [ ! -d 'dotfiles' ]; then git clone git@github.com:joeyschoblaska/dotfiles.git; fi"
   runner "cd dotfiles && git pull origin master && /bin/bash -l -c 'ruby install.rb'"
