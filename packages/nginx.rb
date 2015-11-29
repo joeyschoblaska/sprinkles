c488cb16a5ce0455e516e9765b55b2040e319178
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-11-29 14:08:04 -0600

    let nginx package take different template options

--- packages/nginx.rb
+++ packages/nginx.rb
@@ -3,29 +3,17 @@ package :nginx do
   template_search_path "templates/nginx"
   @app_name = opts[:app_name]
   @server_name = opts[:server_name]
+  @template_name = opts[:template_name] || "nginx.conf"
+
+  conf_path = "/etc/nginx/sites-available/#{@app_name}"
+  link_path = "/etc/nginx/sites-enabled/#{@app_name}"
 
   apt %w(nginx), sudo: true
-  file "/etc/nginx/sites-available/#{@app_name}", content: render("nginx.conf"), sudo: true
-  runner "ln -s /etc/nginx/sites-available/#{@app_name} /etc/nginx/sites-enabled/#{@app_name}", sudo: true
+  file conf_path, content: render(@template_name), sudo: true
+  runner "[ -f #{link_path} ] || ln -s #{conf_path} #{link_path}", sudo: true
 
   verify do
     has_executable "nginx"
-    has_file "/etc/nginx/sites-available/#{@app_name}"
-  end
-end
-
-package :nginx_static do
-  description "nginx and configuration files for serving static content only"
-  template_search_path "templates/nginx"
-  @app_name = opts[:app_name]
-  @server_name = opts[:server_name]
-
-  apt %w(nginx), sudo: true
-  file "/etc/nginx/sites-available/#{@app_name}", content: render("nginx-static.conf"), sudo: true
-  runner "ln -s /etc/nginx/sites-available/#{@app_name} /etc/nginx/sites-enabled/#{@app_name}", sudo: true
-
-  verify do
-    has_executable "nginx"
-    has_file "/etc/nginx/sites-available/#{@app_name}"
+    has_file link_path
   end
 end
