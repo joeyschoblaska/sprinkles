3b2a89ba5e6cd76504401fbacd0dbe0bfed377c4
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-11-29 14:08:25 -0600

    update chessforge sprinkle

--- sprinkles/chessforge.rb
+++ sprinkles/chessforge.rb
@@ -1,14 +1,18 @@
 require "./bootstrap"
 
-package :chessforge_app_deps do
+package :chessforge_api_deps do
   apt %w(libpq-dev), sudo: true
   runner "mkdir -p /home/deploy/chessforge-api/shared/config"
   runner "touch /home/deploy/chessforge-api/shared/config/secrets.yml"
 end
 
-policy :app_stack, roles: :app do
+package :chessforge_web_deps do
+  runner "mkdir -p /home/deploy/chessforge-web/current/public"
+end
+
+policy :api_stack, roles: :api do
   requires :build_essentials
-  requires :chessforge_app_deps
+  requires :chessforge_api_deps
   requires :git
   requires :rvm
   requires :ruby, ruby_version: "2.2"
@@ -20,11 +24,17 @@ policy :app_stack, roles: :app do
   requires :nginx, app_name: "chessforge-api", server_name: "api.chessforge.com"
 end
 
+policy :web_stack, roles: :web do
+  requires :chessforge_web_deps
+  requires :nginx, app_name: "chessforge-web", server_name: "chessforge.com", template_name: "nginx-static.conf"
+end
+
 deployment do
   delivery :ssh do
     user "deploy"
     forward_agent true
-    role :app, "chessforge"
+    role :api, "chessforge"
+    role :web, "chessforge"
   end
 
   source do
