e1657dbd3059889d2f90f73114d269dad73a6e7b
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-02-05 14:01:32 -0600

    added a remove devbox sprinkle

---
+++ sprinkles/devbox.rb
@@ -0,0 +1,24 @@
+require "./bootstrap"
+
+policy :dev_stack, roles: :devbox do
+  requires :build_essentials
+  requires :authorized_keys, keys: %w(joey)
+  requires :git
+  requires :rvm
+  requires :ruby, ruby_version: "2.3"
+  requires :dotfiles
+end
+
+deployment do
+  delivery :ssh do
+    user "joey"
+    forward_agent true
+    role :app, "devboxname"
+  end
+
+  source do
+    prefix   "/usr/local"
+    archives "/usr/local/sources"
+    builds   "/usr/local/build"
+  end
+end
